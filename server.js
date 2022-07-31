8d5977185604d4cf5fd9a96f545dbc7713ab06d2
masugerencias <yannpoirotsanz@gmail.com>
2022-07-31 09:13:31 +0200

    puerto modificado con variable de enterono en server.js

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.use(express.json());
 
  
 app.use("/", router);
-const port = 5050;
+const port = proces.env.port || 5050;
 app.listen(port, () => console.log(`Servidor escuchando por el puerto ${port}!`));
